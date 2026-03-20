60e8a27e04e0d36e04e2daa9e84e91f793178c68
pradipchand397-svg <pradipchand397@gmail.com>
2026-03-20 23:38:14 +0545

    Initial project setup

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
+urlpatterns = router.urls
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
+urlpatterns = router.urls
---
+++ tasks/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = []
